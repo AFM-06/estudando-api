ec4c37d3d3f01a61fdb4ead41849941bb656ffbb
André <andrefm06@gmail.com>
2026-07-12 23:29:00 -0300

    Add base Model for JSON deserialization.

---
+++ Model.cs
@@ -0,0 +1,8 @@
+namespace APIandre;
+public class Model
+{
+    public required int UserId{ get ; set ;}
+    public required int Id{ get ; set ;}
+    public required string Title{ get ; set ;}
+    public required string Body{ get ; set ;}
+}

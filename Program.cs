70f801fe8d7530f8401c0da4bd8c94e238298683
André <andrefm06@gmail.com>
2026-07-13 00:18:24 -0300

    Mission 2 completed.

--- Program.cs
+++ Program.cs
@@ -4,8 +4,14 @@ public class Program
     static async Task Main(string[] args)
     {
         var service = new ApiClient();
-        Model post = await service.GetPostAsync();
+        Model? post = await service.GetPostAsync();
 
-        Console.WriteLine($"Titulo 1: {post.Title}");
+    if (post is null)
+    {
+        Console.WriteLine("No post found.");
+        return;
+    }
+
+    Console.WriteLine($"Titulo: {post.Title}");
     }
 }

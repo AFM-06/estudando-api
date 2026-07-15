79f307a565d866bd4607a2d584863e89686b437b
André <andrefm06@gmail.com>
2026-07-14 23:20:35 -0300

    Add a few methods and tests all of them (CRUD).

--- ApiClient.cs
+++ ApiClient.cs
@@ -1,35 +1,148 @@
 namespace APIandre;
 using System.Text.Json;
-
+using System.Text;
+using System.Net.Http.Json;
 public class ApiClient
 {
-    public async Task<Model?> GetPostAsync()
+    private readonly string _baseUrl = "https://jsonplaceholder.typicode.com/posts";
+    private static readonly HttpClient _client = new HttpClient();
+    public async Task GetAsync()
     {
-        var request = new HttpRequestMessage(HttpMethod.Get,
-            "https://jsonplaceholder.typicode.com/posts/1");
-        using var client = new HttpClient();
+        Console.WriteLine("-----------------------------");
 
-        HttpResponseMessage response = await client.SendAsync(request);
+        var request = new HttpRequestMessage(HttpMethod.Get,_baseUrl);
 
+        HttpResponseMessage response = await _client.SendAsync(request);
         if (response.IsSuccessStatusCode)
         {
             string json = await response.Content.ReadAsStringAsync();
-            var model = JsonSerializer.Deserialize<Model>(
-                json,
-                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
-            );
+            var models = JsonSerializer.Deserialize<List<Model>>(json,new JsonSerializerOptions { PropertyNameCaseInsensitive = true })?? new List<Model>();
+            Console.WriteLine($"Sucesso na requisição: {response.StatusCode}");
+            foreach(var model in models)
+            {
+                Console.WriteLine($"UserId: {model?.UserId}");
+                Console.WriteLine($"Id: {model?.Id}");
+                Console.WriteLine($"Title: {model?.Title}");
+                Console.WriteLine($"Body: {model?.Body}");
+            }
+            Console.WriteLine("----------------------");
+        }else
+        {
+            Console.WriteLine($"Erro na requisição: {response.StatusCode}");
+        }
+        Console.WriteLine("-----------------------------");
+    }
+    public async Task<Model?> GetAsync_post_one()
+    {
+        Console.WriteLine("-----------------------------");
+        var request = new HttpRequestMessage(HttpMethod.Get,$"{_baseUrl}/1");
+        HttpResponseMessage response = await _client.SendAsync(request);
+        if (response.IsSuccessStatusCode)
+        {
+            string json = await response.Content.ReadAsStringAsync();
+            var model = JsonSerializer.Deserialize<Model>(json, new JsonSerializerOptions{PropertyNameCaseInsensitive = true});
+            Console.WriteLine($"Sucesso na requisição: {response.StatusCode}");
+            Console.WriteLine("-----------------------------");
+            return model;
+        }
+        else
+        {
+            Console.WriteLine($"Erro na requisição: {response.StatusCode}");
+            Console.WriteLine("-----------------------------");
+            return null;
+        }
 
-            if(model != null)
-            {
-                Console.WriteLine($"Sucesso na requisição: {response.StatusCode}");
-                return model;
-            }
-            else
-            {
-                Console.WriteLine($"Erro na requisição: {response.StatusCode}");
-            }
+    }
+    public async Task PostAsync(Model post)
+    {
+        Console.WriteLine("-----------------------------");
+
+        string json = JsonSerializer.Serialize(post);
+        using var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var request = new HttpRequestMessage(HttpMethod.Post, _baseUrl)
+        {
+            Content = content
+        };
+
+        HttpResponseMessage response = await _client.SendAsync(request);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Post feito ({response.StatusCode}) {responseBody}.");
+        }
+        else
+        {
+            Console.WriteLine($"Erro {response.StatusCode}");
+        }
+        Console.WriteLine("-----------------------------");
+    }
+    public async Task DeleteAsync(int delete)
+    {
+        Console.WriteLine("-----------------------------");
+
+        var request = new HttpRequestMessage(HttpMethod.Delete, $"{_baseUrl}/{delete}");
+        HttpResponseMessage response = await _client.SendAsync(request);
+
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine($"{response.StatusCode}");
+            Console.WriteLine($"O Id {delete} foi deletado.");
+        }
+        else
+        {
+            Console.WriteLine($"Erro: {response.StatusCode}");
+        }
+        Console.WriteLine("-----------------------------");
+    }
+    public async Task PutAsync(int id, Model put)
+    {
+        Console.WriteLine("-----------------------------");
+
+        string json = JsonSerializer.Serialize(put);
+        using var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var request = new HttpRequestMessage(HttpMethod.Put, $"{_baseUrl}/{id}")
+        {
+          Content = content
+        };
+
+        HttpResponseMessage response = await _client.SendAsync(request);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Codigo: {response.StatusCode} / {responseBody}");
 
         }
-        return null;
+        else
+        {
+            Console.WriteLine($"Erro: {response.StatusCode}.");
+        }
+        Console.WriteLine("-----------------------------");
+    }
+    public async Task PatchAsync(int id, object atualizado)
+    {
+        Console.WriteLine("-----------------------------");
+        string json = JsonSerializer.Serialize(atualizado);
+        using var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var request = new HttpRequestMessage(HttpMethod.Patch, $"{_baseUrl}/{id}")
+        {
+            Content = content
+        };
+        HttpResponseMessage response = await _client.SendAsync(request);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Codigo: {response.StatusCode} / Id: {id} atualizado {responseBody}");
+        }
+        else
+        {
+            Console.WriteLine($"Erro: {response.StatusCode}.");
+        }
+        Console.WriteLine("-----------------------------");
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,14 +4,26 @@ public class Program
     static async Task Main(string[] args)
     {
         var service = new ApiClient();
-        Model? post = await service.GetPostAsync();
-
-    if (post is null)
-    {
-        Console.WriteLine("No post found.");
-        return;
-    }
-
-    Console.WriteLine($"Titulo: {post.Title}");
+        Model? getOne = await service.GetAsync_post_one();
+        Console.WriteLine(getOne?.Title);
+        //await service.GetAsync();
+        Model post = new()
+        {
+            UserId = 1,
+            Id = 1,
+            Title = "Testando Post",
+            Body = "Testando Post"
+        };
+        await service.PostAsync(post);
+        await service.DeleteAsync(2);
+        Model put = new()
+        {
+            UserId = 1,
+            Id = 1,
+            Title = "Testando Put",
+            Body = "Testando Put"
+        };
+        await service.PutAsync(1,put);
+        await service.PatchAsync(1,new {id = 3});
     }
 }
